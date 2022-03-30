d500483d9574ba8e77820dcbf8ec39d90334d607
carlosBorregoSierra <ingcarlosBorrego@gmail.com>
2022-03-29 22:10:42 -0500

    Validamos agregar  arrays al carrito y suma las cantiudades del mismo producto

--- carrito/src/App.js
+++ carrito/src/App.js
@@ -10,16 +10,40 @@ class App extends Component {
       {name: 'Tomate', price: 1500, img: '/productos/tomate.jpg'},
       {name: 'Arberja', price: 2500, img: '/productos/arberja.jpg'},
       {name: 'Lechuga', price: 500, img: '/productos/lechuga.jpg'},
-    ]
+    ],
+    carro: [
+      //{name: 'Lechuga', price: 500, cantidad: 1, img: '/productos/lechuga.jpg'},
+    ],
   }
+
+  agregarAlCarro = (producto) =>{
+      const {carro} = this.state;
+      if(carro.find(x => x.name === producto.name)){
+        const newCarro = carro.map(x => x.name === producto.name
+          ? ({
+            ...x,
+            cantidad: x.cantidad + 1
+          })
+          :x)
+        return this.setState({ carro: newCarro})
+      }
+      return this.setState({
+        carro: this.state.carro.concat({
+          ...producto,
+          cantidad: 1,
+        })
+      })
+  }
+
   render(){
+    console.log(this.state.carro);
     return (
       <div>
         <Navbar/>
         <Layout>
           <Title/>
           <Productos
-          agregarAlCarro={()=>console.log('No hace nada')}
+          agregarAlCarro={this.agregarAlCarro}
           productos={this.state.productos}
           />
           </Layout>
